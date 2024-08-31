1d3b88d85912ccfe57484cb60831089902ded76b
mhastin <mhastiningrat@gmail.com>
2024-08-31 12:36:10 +0700

    logo in login page

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import env from "../../config/environment";
 import Loading from '../Utilities/Loading';
+import logods from "../../media/logods.png"
 
 const Login = () => {
 
@@ -40,8 +41,8 @@ const Login = () => {
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-                    Flowbite    
+                    <img className="w-8 h-8 mr-2" src={logods} alt="logo"/>
+                    Double M Stationary    
                 </a>
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
